aa7f92bd1b96cb9b1e956c07ed4308a127168b6c
Biswajit <paridabiswa2k@gmail.com>
2025-09-11 19:06:08 +0530

    adding auth context

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   <StrictMode>
+    <AuthProvider>
+
     <App />
+    </AuthProvider>
   </StrictMode>
   </BrowserRouter>
 )
