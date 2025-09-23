43369479b58f03333275428845ee2187cd53a44b
Biswajit <paridabiswa2k@gmail.com>
2025-09-23 20:13:05 +0530

    add submit feature api

--- my-app/src/user/SubmitTask.tsx
+++ my-app/src/user/SubmitTask.tsx
@@ -1,20 +1,39 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 
 import type { TaskCardProps } from "../task/TaskCard";
+import { AuthContext } from "../context/AuthContext";
 
 function SubmitTask({task}:{task:TaskCardProps}){
     
     const [github_link,setGithub_link]= useState<string>("");
+    const auth = useContext(AuthContext);
+    const jwt = auth?.jwt;
+    
 
 
     // requestmapping =api/submissions
 // 1-POST  
 //   task_id,github_link,jwt   submittask
 
-const handleSubmitTask=(e:React.InputEvent<HTMLFormElement>)=>{
+const handleSubmitTask=async(e:React.InputEvent<HTMLFormElement>)=>{
 
     e.preventDefault();
-    const task_id = task
+    const task_id = task.id;
+    try {
+        const response = await fetch(`http://localhost:5000/api/submissions?task_id=${task_id}&github_link=${github_link}`,{
+            method:"POST",
+            headers:{
+                "Authorization":`Bearer ${jwt}`,
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(github_link)
+        })
+        const result = await response.json();
+        console.log(result)
+        
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
